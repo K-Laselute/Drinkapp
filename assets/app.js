2bba2111b5855e0667bc207bdac0befb81a4a34c
Kyle Laselute <klaselute@gmail.com>
2019-11-01 14:18:39 -0600

    Added script to grab ingredients and thumbnails from the drink DB api. Created a table to hold ingredients in html.

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,35 @@
 const drinkButton = document.getElementById('randomButton');
 const image = document.getElementById('drinkImage');
+const instructions = document.getElementById('instructions')
 
-fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-    .then(response => response.json())
-    .then(data => getImage(data))
+
+// reusable fetch function
+
+function fetchData(url) {
+    return fetch(url)
+        .then(response => response.json())
+}
+
+fetchData('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(data => getInstructions(data.drinks[0].strInstructions))
+
+fetchData('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(data => getImage(data.drinks[0].strDrinkThumb))
+
+
+// base functions
 
 function getImage(data) {
-    console.log(data.drinks[0].strDrinkThumb);
     const html = `
-        <img src='${data.drinks[0].strDrinkThumb}' alt>
-    `;
+        <img src='${data}' alt class="img-fluid">
+        `;
     image.innerHTML = html;
 }
 
-drinkButton.addEventListener('click', (data) => {
-    console.log(data.drinks[0].strDrinkThumb);
-});
+function getInstructions (data) {
+    console.log(data);
+    const info = `
+        <p>${data}</p>
+    `;
+    instructions.innerHTML = info;
+}
